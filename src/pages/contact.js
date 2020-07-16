5cda33806ed1f9a01d8948bc03bae16eaf46456f
Jose Navas <jesquivelnavas@gmail.com>
2020-07-15 22:04:10 -0600

    Adds contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,123 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers"
+import { object, string } from "yup"
+import TextField from "@material-ui/core/TextField"
+import Grid from "@material-ui/core/Grid"
+import Paper from "@material-ui/core/Paper"
+import Box from "@material-ui/core/Box"
+import Button from "@material-ui/core/Button"
+import Grow from "@material-ui/core/Grow"
+import Container from "@material-ui/core/Container"
+import { makeStyles } from "@material-ui/core/styles"
 
 import Layout from "../components/layout"
+import chat from "../assets/images/chat.svg"
+
+const schema = object().shape({
+  email: string().required().email(),
+})
+
+const sendEmail = data => {
+  console.log(data)
+}
+
+const useStyles = makeStyles(theme => ({
+  typography: {
+    textAlign: "center",
+    [theme.breakpoints.up("lg")]: {
+      textAlign: "left",
+    },
+  },
+  imgContainer: {
+    textAlign: "center",
+  },
+  paper: {
+    padding: theme.spacing(3),
+  },
+}))
 
 const Contact = () => {
+  const classes = useStyles()
+
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  })
+
   return (
     <Layout>
-      <main>contact form</main>
+      <main>
+        <Container>
+          <Paper elevation={3} className={classes.paper}>
+            <Grid container spacing={5} justify="center" alignItems="center">
+              <Grid item xs={12} lg={6}>
+                <form onSubmit={handleSubmit(sendEmail)} action="">
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} lg={6}>
+                      <TextField
+                        fullWidth
+                        name="name"
+                        label="Name"
+                        inputRef={register}
+                        error={errors.name}
+                        helperText={errors.name?.message}
+                      />
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                      <TextField
+                        fullWidth
+                        name="email"
+                        label="Email"
+                        inputRef={register}
+                        error={errors.email}
+                        helperText={errors.email?.message}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        fullWidth
+                        name="subject"
+                        label="Subject"
+                        inputRef={register}
+                        error={errors.subject}
+                        helperText={errors.subject?.message}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        multiline
+                        fullWidth
+                        label="Message"
+                        name="message"
+                        rows={4}
+                        inputRef={register}
+                        error={errors.message}
+                        helperText={errors.sbuject?.message}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Box display="flex" justifyContent="end">
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          color="primary"
+                        >
+                          Send
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </form>
+              </Grid>
+              <Grid item xs={12} sm={6} className={classes.imgContainer}>
+                <Grow in timeout={1000}>
+                  <img src={chat} alt="chat" />
+                </Grow>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Container>
+      </main>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import Button from "@material-ui/core/Button"
+import Paper from "@material-ui/core/Paper"
 import Container from "@material-ui/core/Container"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
@@ -20,6 +21,9 @@ const useStyles = makeStyles(theme => ({
   imgContainer: {
     textAlign: "center",
   },
+  paper: {
+    padding: theme.spacing(3),
+  },
 }))
 
 const ContactButton = () => (
@@ -35,39 +39,41 @@ export default function Home() {
     <Layout>
       <main>
         <Container>
-          <Grid container spacing={3} justify="center" alignItems="center">
-            <Grid item xs={12} lg={6}>
-              <Typography
-                gutterBottom
-                variant="h2"
-                component="h1"
-                color="textPrimary"
-                className={classes.typography}
-              >
-                Hi, I'm Jonathan a web developer
-              </Typography>
-              <Typography
-                gutterBottom
-                component="p"
-                variant="subtitle1"
-                color="textSecondary"
-                className={classes.typography}
-              >
-                Full Stack / Freelancer
-              </Typography>
-              <Hidden mdDown>
-                <ContactButton />
-              </Hidden>
+          <Paper elevation={3} className={classes.paper}>
+            <Grid container spacing={3} justify="center" alignItems="center">
+              <Grid item xs={12} lg={6}>
+                <Typography
+                  gutterBottom
+                  variant="h2"
+                  component="h1"
+                  color="textPrimary"
+                  className={classes.typography}
+                >
+                  Hi, I'm Jonathan a web developer
+                </Typography>
+                <Typography
+                  gutterBottom
+                  component="p"
+                  variant="subtitle1"
+                  color="textSecondary"
+                  className={classes.typography}
+                >
+                  Full Stack / Freelancer
+                </Typography>
+                <Hidden mdDown>
+                  <ContactButton />
+                </Hidden>
+              </Grid>
+              <Grid item xs={12} sm={6} className={classes.imgContainer}>
+                <Grow in timeout={1000}>
+                  <img src={avatar} alt="avatar" />
+                </Grow>
+                <Hidden lgUp>
+                  <ContactButton />
+                </Hidden>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={6} className={classes.imgContainer}>
-              <Grow in timeout={1000}>
-                <img src={avatar} alt="avatar" />
-              </Grow>
-              <Hidden lgUp>
-                <ContactButton />
-              </Hidden>
-            </Grid>
-          </Grid>
+          </Paper>
         </Container>
       </main>
     </Layout>
